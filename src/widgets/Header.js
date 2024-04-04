da45cc81a20fbe6d37015b8368cddcf5ddc085e1
Anton-Dmitriichuk <dmtant26@gmail.com>
2024-04-04 17:56:19 +0300

    add:Header

--- src/widgets/Header.js
+++ src/widgets/Header.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+import { P } from '../app/TextTags';
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <HeaderWr>
+      <DivHeaderContainerServices>
+        <P>О ЖК</P>
+        <P>Услуги</P>
+        <P>Подбор жилья</P>
+        <P>Контакты</P>
+      </DivHeaderContainerServices>
+
+      <DivHeaderContainerContacts>
+        
+      </DivHeaderContainerContacts>
+    </HeaderWr>
   );
 };
+
+const DivHeaderContainerServices = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.11vw;
+`
+const DivHeaderContainerContacts = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.11vw;
+`
+
+const HeaderWr = styled.header`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 99;
+  border-radius: 20px;
+  background-color: white;
+
+`
 
 export default Header;
