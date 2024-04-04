696efbfa3af97192c86175c12c251e512d92415e
Felirian <141245468+Felirian@users.noreply.github.com>
2024-04-04 19:23:18 +0300

    add: crad apartt

--- src/shared/ApartCard.js
+++ src/shared/ApartCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {P} from "../app/TextTags";
+import {H2, H4, P} from "../app/TextTags";
 import styled from "styled-components";
 import {BREAKPOINTS} from "../app/variables";
 
@@ -11,32 +11,41 @@ const ApartCard = ({id, Rooms, Cost, Square, Floor, apartNumber, Entrance}) => {
         alt={`item_${id}`}
         width={400}
         height={400}
-        //style={{zIndex: 0}}
       />
-      <P>{Rooms == 0 ? 'Студия' : `${Rooms} комнатная`}</P>
-      <P>Стоимость: {Cost}</P>
-      <P>Площадь: {Square}</P>
-      <P>Этаж: {Floor}</P>
-      <P>Номер кв: {apartNumber}</P>
-      <P>Секция: { Entrance}</P>
+      <RoomsWr>
+        <P>{Rooms == 0 ? 'Студия' : `${Rooms} комнатная`}</P>
+      </RoomsWr>
+
+      <P>Цена: {Cost}₽</P>
+      <H4>{Square} m²</H4>
+      <P>{Floor}-й этаж</P>
     </ApartCardWr>
   );
 };
 const ApartCardWr = styled.div`
   width: 22%;
   display: flex;
+  
+  gap: 1vw;
   flex-direction: column;
   padding: 1vw;
   background-color: #ece7e7;
   border-radius: 3vw;
+  position: relative;
   @media ${BREAKPOINTS.mobile} {
     width: 45%;
   }
 `
-const Rooms = styled.div`
+const RoomsWr = styled.div`
+  position: absolute;
+  display: flex;
+  padding: 1vw;
   
+  background-color: white;
+  border-radius: 100vw;
 `
 const Image = styled.img`
+  margin-top: 4vw;
   width: 100%;
   height: auto;
 `
